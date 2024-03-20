db37c8bdf25c80a6bfa2fb783ae1604053e3b202
sean20020313 <sean20020313@gmail.com>
2024-03-20 21:58:34 +0800

    update

--- src/Form_xx.jsx
+++ src/Form_xx.jsx
@@ -1,14 +1,49 @@
 import { useState } from 'react';
 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { supabase } from './db/SupabaseClient';
+
+import { toast } from 'react-toastify';
+
 const Form_xx = () => {
   const [newItemName, setNewItemName] = useState('');
 
+  const queryClient = useQueryClient();
+
+  const { mutate: createTask, isLoading } = useMutation({
+    mutationFn: async (title) => {
+      try {
+        const { data, error } = await supabase
+          .from('task_40')
+          .insert([{ title, is_done: false }])
+          .select();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('task added');
+      setNewItemName('');
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (newItemName === '') {
+      toast.warning('please provide title');
+    } else {
+      createTask(newItemName);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
-      <h4>task bud -- htchung</h4>
+      <h4>task bud -- sean 211418040</h4>
       <div className='form-control'>
         <input
           type='text '
