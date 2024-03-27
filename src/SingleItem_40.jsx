8ad5ce20a8f25a681ae5b321f99386961548c25a
sean20020313 <sean20020313@gmail.com>
2024-03-27 21:11:11 +0800

    update

--- src/SingleItem_40.jsx
+++ src/SingleItem_40.jsx
@@ -1,15 +1,62 @@
-const SingleItem = ({ item }) => {
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { supabase } from './db/SupabaseClient';
+import { toast } from 'react-toastify';
+
+const SingleItem_40 = ({ item }) => {
+  const queryClient = useQueryClient();
+
+  // Edit Task Mutation
+  const { mutate: editTask } = useMutation({
+    mutationFn: async ({ taskId, isDone }) => {
+      try {
+        await supabase
+          .from('task_40')
+          .update({ is_done: isDone })
+          .eq('id', taskId);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('Task updated');
+    },
+  });
+
+  // Delete Task Mutation
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: async ({ taskId }) => {
+      try {
+        await supabase.from('task_40').delete().eq('id', taskId);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['tasks'],
+      });
+      toast.success('Task deleted');
+    },
+  });
+
   return (
     <div className='single-item'>
       <input
         type='checkbox'
-        checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        checked={item.is_done}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.is_done })}
       />
       <p
         style={{
           textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textDecoration: item.is_done ? 'line-through' : 'none',
         }}
       >
         {item.title}
@@ -17,11 +64,11 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => deleteTask({ taskId: item.id })} // 修正传递参数的方式
       >
         delete
       </button>
     </div>
   );
 };
-export default SingleItem;
+export default SingleItem_40;
